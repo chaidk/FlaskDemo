136e021c04027dd73c57c60058e048ad52735018
chai <chaidk.1992@gmail.com>
2019-10-21 15:32:05 +0800

    10.21

--- flaskdemo1.py
+++ flaskdemo1.py
@@ -12,7 +12,7 @@ def index():
 
 @app.route('/login',methods=['GET','POST'])
 def login():
-    if request.method==["GET"]:
+    if request.method=="GET":
         return render_template('login.html')
     else:
         user = request.form.get('user')
